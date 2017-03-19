245bb6eef7a4632f9f017ecd544b3c1434f922cd
Dhanush123 <dhanush.patel@ymail.com>
2017-03-19 03:51:25 -0700

    salads
    sdasdasd

--- index.js
+++ index.js
@@ -3,37 +3,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const Report = require('ga-report');
+const Report = require("ga-report");
 
 const restService = express();
 restService.use(bodyParser.json());
 
-// var report;
+var report;
+// report.once("ready", function() {
+//   console.log("authenticated now!!!!");
+// });
 
 var numNewUsers = 0;
 
 var options = {
-  'ids': 'ga:123456-UA',
-  'start-date': '2017-1-01',
-  'end-date': '2017-11-31',
-  'metrics': 'ga:organicSearches,ga:percentNewSessions,ga:sessions,ga:avgSessionDuration,ga:newUsers,ga:1dayUsers,ga:30dayUsers,ga:7dayUsers,ga:pageviews'
+  "ids": "ga:123456-UA",
+  "start-date": "2017-1-01",
+  "end-date": "2017-11-31",
+  "metrics": "ga:organicSearches,ga:percentNewSessions,ga:sessions,ga:avgSessionDuration,ga:newUsers,ga:1dayUsers,ga:30dayUsers,ga:7dayUsers,ga:pageviews"
 };
 
 restService.get("/", function (req, res) {
   console.log("hook request");
   try {
       if (req) {
-        // report = new Report({'username':'jessegao12@gmail.com', 'password':'happyman'});
-        // report.once('ready', function() {
-        //   console.log("authenticated now!!!!");
-        // });
         if(req.qtype == "newusers"){
-          // newUsersFind(req, function(result) {
-          //            //callback is ultimately to return Messenger appropriate responses formatted correctly
-          //              return res.json({
-          //                message: "You have " + numNewUsers + " new user(s)!"
-          //              });
-          //          });
+          report = new Report({"username":"jessegao12@gmail.com", "password":"happyman"});
+          newUsersFind(req, function(result) {
+                     //callback is ultimately to return Messenger appropriate responses formatted correctly
+                       return res.json({
+                         message: "You have " + numNewUsers + " new user(s)!"
+                       });
+                   });
         }
       }
   }
@@ -49,10 +49,14 @@ restService.get("/", function (req, res) {
 });
 
 function newUsersFind(req,callback){
-  // report.get(options, function(err, data) {
-  //   if (err) console.error(err);
-  //   console.log(data);
-  //   numNewUsers = data.totalsForAllResults["ga:newUsers"];
-  //   callback();
-  // });
+  report.get(options, function(err, data) {
+    if (err) console.error(err);
+    console.log(data);
+    numNewUsers = data.totalsForAllResults["ga:newUsers"];
+    callback();
+  });
 }
+
+restService.listen((process.env.PORT || 8000), function () {
+  console.log('Server listening');
+});
